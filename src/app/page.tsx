7fcdc5a9146c1c2fb6d77a7522ffb3432ba0283a
Mauri <mauri.monzon91@gmail.com>
2024-04-12 13:54:20 -0300

    Config de la fuente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { titleFont } from "@/config/fonts";
 
 export default function Home() {
-
   return (
-    <div >
-     <h1>Hola</h1>
+    <div>
+      <h1>Hola</h1>
+      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
+      <h1 className={`${titleFont.className}`}>Hola mundo</h1>
     </div>
   );
 }

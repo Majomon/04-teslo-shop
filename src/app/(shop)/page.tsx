6c50a710f65e1de052639d6d5313d1460f5ee6f3
Mauri <mauri.monzon91@gmail.com>
2024-04-12 15:18:27 -0300

    Compojente title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,9 @@
-import { titleFont } from "@/config/fonts";
+import { Title } from "@/components";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hola</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
-      <h1 className={`${titleFont.className}`}>Hola mundo</h1>
-    </div>
+    <>
+      <Title title="Tienda" subTitle="Todos los productos" className="mb-2" />
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export * from "./ui/Top-menu/TopMenu";
 export * from "./ui/NotFound/NotFound";
+export * from "./ui/Title/Title";
+

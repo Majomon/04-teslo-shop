47740f2669d90ab69d4ea2a1b388ee498694bab0
Mauri <mauri.monzon91@gmail.com>
2024-04-12 21:58:31 -0300

    Primera parte del slideShow

--- src/components/product/SlideShow/ProductSlideShow.tsx
+++ src/components/product/SlideShow/ProductSlideShow.tsx
@@ -1,6 +1,14 @@
 "use client";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
 import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { Swiper as SwiperObject } from "swiper";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./slideshow.css";
+import Image from "next/image";
 
 interface Props {
   images: string[];
@@ -9,18 +17,33 @@ interface Props {
 }
 
 export const ProductSlideShow = ({ images, title, className }: Props) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
   return (
     <div className={className}>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        style={
+          {
+            "--swiper-navigation-color": "#fff",
+            "--swiper-pagination-color": "#fff",
+          } as React.CSSProperties
+        }
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {images.map((image) => (
+          <SwiperSlide key={image}>
+            <Image
+              width={1024}
+              height={800}
+              src={`/products/${image}`}
+              alt={title}
+              className="rounded-lg "
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );

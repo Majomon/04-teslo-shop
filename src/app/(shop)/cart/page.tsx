425ad5cd3acd51dc30e46248fe4e8ad2a5addf19
Mauri <mauri.monzon91@gmail.com>
2024-04-13 18:15:25 -0300

    Pequeño cambio en carrito

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -48,7 +48,7 @@ export default function CartPage() {
           </div>
 
           {/* Checkout - Resumen*/}
-          <div className="rounded-xl bg-white p-7 shadow-xl">
+          <div className="h-fit rounded-xl bg-white p-7 shadow-xl">
             <h2 className="mb-2 text-2xl">Resumen de orden</h2>
             <div className="grid grid-cols-2">
               <span>No. Productos</span>

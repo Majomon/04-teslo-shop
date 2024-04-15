69dd3b3b75a488d0d0ac68a15a7659bbbb567a2a
Mauri <mauri.monzon91@gmail.com>
2024-04-15 14:14:29 -0300

    Paginacion

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,13 +1,18 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 // import { initialData } from "@/seed/seed";
-
 // const products = initialData.products;
 
-export default async function Home() {
-  const { products } = await getPaginatedProductsWithImages();
-  console.log(products);
-  
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products } = await getPaginatedProductsWithImages({page});
+
   return (
     <>
       <Title title="Tienda" subTitle="Todos los productos" className="mb-2" />

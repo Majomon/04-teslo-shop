1cc15b6eef56ec828bf6b0ac78df9694ddf37405
Mauri <mauri.monzon91@gmail.com>
2024-04-15 14:19:23 -0300

    Controlado error de pag no existe

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
+import { redirect } from "next/navigation";
 // import { initialData } from "@/seed/seed";
 // const products = initialData.products;
 
@@ -11,7 +12,11 @@ interface Props {
 
 export default async function Home({ searchParams }: Props) {
   const page = searchParams.page ? parseInt(searchParams.page) : 1;
-  const { products } = await getPaginatedProductsWithImages({page});
+  const { products } = await getPaginatedProductsWithImages({ page });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
 
   return (
     <>

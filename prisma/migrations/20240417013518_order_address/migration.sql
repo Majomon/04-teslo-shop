b0e3c24eaa479ffcc007dde441fde4e1322d4f28
Mauri <mauri.monzon91@gmail.com>
2024-04-16 22:37:12 -0300

    Actualizado seed

---
+++ prisma/migrations/20240417013518_order_address/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `OrderItems` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "OrderItems" DROP CONSTRAINT "OrderItems_orderId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderItems" DROP CONSTRAINT "OrderItems_productId_fkey";
+
+-- DropTable
+DROP TABLE "OrderItems";
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "size" "Size" NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

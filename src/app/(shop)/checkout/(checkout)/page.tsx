9434d18fff9ec5d6cf952f6e5c169d20744a9ab2
Mauri <mauri.monzon91@gmail.com>
2024-04-16 21:59:20 -0300

    productincar checkout

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,13 +1,8 @@
-import { QuantitySelector, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+import { Title } from "@/components";
 import Image from "next/image";
 import Link from "next/link";
+import { ProductsInCartCheckout } from "./ui/ProductsInCartCheckout";
 
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-];
 export default function CheckoutPage() {
   return (
     <div className="mb-72 flex items-center justify-center px-10 sm:px-0">
@@ -22,26 +17,7 @@ export default function CheckoutPage() {
             </Link>
 
             {/* Items */}
-            {productsInCart.map((product) => (
-              <div key={product.slug} className="mb-5 flex">
-                <Image
-                  src={`/products/${product.images[0]}`}
-                  alt={product.title}
-                  width={100}
-                  height={100}
-                  className="mr-5 rounded"
-                  style={{
-                    width: "100px",
-                    height: "100px",
-                  }}
-                />
-                <div>
-                  <p>{product.title}</p>
-                  <p>{product.price} x 3</p>
-                  <p className="font-bold">Subtotal: ${product.price * 3}`</p>
-                </div>
-              </div>
-            ))}
+            <ProductsInCartCheckout />
           </div>
 
           {/* Checkout - Resumen*/}
